bb3e0d765ebd97788ea068bf45f0f02585231e26
Kamil Monicz <kamil@monicz.pl>
2020-07-14 01:08:06 +0200

    Added DefaultIfEmptyF

---
+++ Tests/DefaultIfEmptyTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using JM.LinqFaster;
+using System.Linq;
+using static Tests.Test;
+
+namespace Tests
+{
+    [TestFixture]
+    class DefaultIfEmptyTests {
+
+        [Test]
+        public void DefaultIfEmptyArray() {
+            var a = intArray.DefaultIfEmptyF();
+            var b = intArray.DefaultIfEmpty();
+
+            Assert.That(a, Is.EqualTo(b));
+        }
+
+        [Test]
+        public void DefaultIfEmptyList() {
+            var a = intList.DefaultIfEmptyF();
+            var b = intList.DefaultIfEmpty();
+
+            Assert.That(a, Is.EqualTo(b));
+        }
+    }
+}
