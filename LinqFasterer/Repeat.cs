f5bf380cdfde4ff624660158f807d81d51cb4885
Kamil Monicz <kamil@monicz.pl>
2020-12-23 15:07:10 +0100

    RepeatF optimization, Reduced requirements

--- LinqFasterer/Repeat.cs
+++ LinqFasterer/Repeat.cs
@@ -14,9 +14,25 @@ namespace LinqFasterer
         /// <returns>An <see cref="IList{T}"/> that contains a repeated <paramref name="element"/>.</returns>
         public static IList<TResult> RepeatF<TResult>(TResult element, int count)
         {
+            if (count < 1)
+                return EmptyF<TResult>();
+            
             var result = new TResult[count];
+            var filled = Math.Min(count, 16);
+            
+            // It's more efficient to fill some initial data by index instead of calling Array.Copy straightaway
+            // The magic number is a result of multiple benchmark tests
+            for (var i = 0; i < filled; i++)
+                result[i] = element;
 
-            Array.Fill(result, element);
+            // ReSharper disable once InvertIf
+            if (filled < count)
+            {
+                for (; filled * 2 < count; filled *= 2)
+                    Array.Copy(result, 0, result, filled, filled);
+
+                Array.Copy(result, 0, result, filled, count - filled);
+            }
 
             return result;
         }
