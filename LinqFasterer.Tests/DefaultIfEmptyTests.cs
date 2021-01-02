c8826bf9a64f6bd5064d6cf53f6f48473580aa3f
Kamil Monicz <kamil@monicz.pl>
2021-01-02 23:54:24 +0100

    Improved DefaultIfEmptyF test code coverage

--- LinqFasterer.Tests/DefaultIfEmptyTests.cs
+++ LinqFasterer.Tests/DefaultIfEmptyTests.cs
@@ -1,14 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using LinqFasterer.Shared;
 using Xunit;
 
 namespace LinqFasterer.Tests
 {
     public partial class Test
     {
+        [Theory]
+        [Trait(nameof(EnumerableF.DefaultIfEmptyF), null)]
+        [MemberData(nameof(Utilities.TestArray), typeof(int), 5, 0, 100, MemberType = typeof(Utilities))]
+        public void DefaultIfEmptyTest_Int(IList<int> source)
+        {
+            var expected = source.DefaultIfEmpty().ToArray();
+            var actual = source.DefaultIfEmptyF().ToArrayF();
+
+            Assert.Equal(expected, actual);
+        }
+        
         [Fact]
         [Trait(nameof(EnumerableF.DefaultIfEmptyF), null)]
-        public void DefaultIfEmptyTest_Int()
+        public void DefaultIfEmptyTest_IntEmpty()
         {
             var source = Array.Empty<int>();
 
@@ -20,7 +33,7 @@ namespace LinqFasterer.Tests
 
         [Fact]
         [Trait(nameof(EnumerableF.DefaultIfEmptyF), null)]
-        public void DefaultIfEmptyTest_IntDefault()
+        public void DefaultIfEmptyTest_IntEmptyDefault()
         {
             var source = Array.Empty<int>();
 
