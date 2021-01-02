6c37862f50d6925425ba0c5ac566c99b31eac664
Kamil Monicz <kamil@monicz.pl>
2021-01-02 18:34:53 +0100

    Added RepeatSequenceBenchmark

---
+++ LinqFasterer.Benchmark/Benchmarks/RepeatSequenceBenchmark.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace LinqFasterer.Benchmarks.Benchmarks
+{
+    public class RepeatSequenceBenchmark : Benchmarkable
+    {
+        [Benchmark(Baseline = true)]
+        public int[] RepeatSequenceFaster()
+        {
+            return EnumerableF.RepeatSequenceF(new[] {1, 2, 3, 4, 5}, N).ToArrayF();
+        }
+    }
+}
