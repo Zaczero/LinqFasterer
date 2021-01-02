af1f57b4e4f370e3f0c81b60bbefd2aab9974f6b
Kamil Monicz <kamil@monicz.pl>
2021-01-02 18:09:04 +0100

    Added RepeatSequenceF

--- LinqFasterer.Tests/RepeatTests.cs
+++ LinqFasterer.Tests/RepeatTests.cs
@@ -9,14 +9,30 @@ namespace LinqFasterer.Tests
     {
         [Theory]
         [Trait(nameof(EnumerableF.RepeatF), null)]
+        [InlineData(new int[0])]
         [MemberData(nameof(Utilities.TestArray), typeof(int), 5, 0, 100, MemberType = typeof(Utilities))]
         [MemberData(nameof(Utilities.TestArray), typeof(int), 10, 0, 2, MemberType = typeof(Utilities))]
         [MemberData(nameof(Utilities.TestArray), typeof(int), 50, 0, 5, MemberType = typeof(Utilities))]
         [MemberData(nameof(Utilities.TestArray), typeof(int), 5000, 0, 100, MemberType = typeof(Utilities))]
         public void RepeatTest_Int(IList<int> source)
         {
-            var expected = Enumerable.Repeat(source[0], source.Count).ToArray();
-            var actual = EnumerableF.RepeatF(source[0], source.Count).ToArrayF();
+            var expected = Enumerable.Repeat(source.FirstOrDefault(), source.Count).ToArray();
+            var actual = EnumerableF.RepeatF(source.FirstOrDefault(), source.Count).ToArrayF();
+
+            Assert.Equal(expected, actual);
+        }
+        
+        [Theory]
+        [Trait(nameof(EnumerableF.RepeatSequenceF), null)]
+        [InlineData(new[] {1, 2, 3, 1, 2, 3}, new [] {1, 2, 3}, 2)]
+        [InlineData(new[] {1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3}, new [] {1, 2, 3}, 4)]
+        [InlineData(new[] {1, 1, 1, 1, 1}, new [] {1}, 5)]
+        [InlineData(new int[0], new [] {1}, 0)]
+        [InlineData(new int[0], new int[0], 5)]
+        [InlineData(new[] {1}, new [] {1}, 1)]
+        public void RepeatSequenceTest_Int(int[] expected, int[] source, int count)
+        {
+            var actual = EnumerableF.RepeatSequenceF(source, count).ToArrayF();
 
             Assert.Equal(expected, actual);
         }
--- LinqFasterer/Repeat.cs
+++ LinqFasterer/Repeat.cs
@@ -17,8 +17,9 @@ namespace LinqFasterer
             if (count < 1)
                 return EmptyF<TResult>();
             
-            var result = new TResult[count];
-            var filled = Math.Min(count, 16);
+            var resultLength = count;
+            var result = new TResult[resultLength];
+            var filled = Math.Min(resultLength, 16);
             
             // It's more efficient to fill some initial data by index instead of calling Array.Copy straightaway
             // The magic number is a result of multiple benchmark tests
@@ -26,13 +27,50 @@ namespace LinqFasterer
                 result[i] = element;
 
             // ReSharper disable once InvertIf
-            if (filled < count)
+            if (filled < resultLength)
             {
-                for (; filled * 2 < count; filled *= 2)
+                for (; filled * 2 < resultLength; filled *= 2)
                     Array.Copy(result, 0, result, filled, filled);
 
-                Array.Copy(result, 0, result, filled, count - filled);
+                Array.Copy(result, 0, result, filled, resultLength - filled);
             }
+
+            return result;
+        }
+        
+        /// <summary>
+        /// Generates a sequence that contains one repeated sequence.
+        /// </summary>
+        /// <param name="source">An <see cref="IList{T}"/> to be repeated.</param>
+        /// <param name="count">The number of times to repeat the <paramref name="source"/> in the generated sequence.</param>
+        /// <typeparam name="TResult">The type of the elements of <paramref name="source"/>.</typeparam>
+        /// <returns>An <see cref="IList{T}"/> that contains a repeated <paramref name="source"/>.</returns>
+        public static IList<TResult> RepeatSequenceF<TResult>(IList<TResult> source, int count)
+        {
+            if (count < 1)
+                return EmptyF<TResult>();
+
+            var sourceArray = source.ToArrayF();
+            var sourceLength = sourceArray.Length;
+            
+            switch (sourceLength)
+            {
+                case 0:
+                    return EmptyF<TResult>();
+                case 1:
+                    return RepeatF(sourceArray[0], count);
+            }
+
+            var resultLength = sourceLength * count;
+            var result = new TResult[resultLength];
+            
+            Array.Copy(sourceArray, 0, result, 0, sourceLength);
+            var filled = sourceLength;
+            
+            for (; filled * 2 < resultLength; filled *= 2)
+                Array.Copy(result, 0, result, filled, filled);
+
+            Array.Copy(result, 0, result, filled, resultLength - filled);
 
             return result;
         }
