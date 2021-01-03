a05bfb842e7a1347e85787c208495d2914f77591
Kamil Monicz <kamil@monicz.pl>
2021-01-03 20:25:21 +0100

    Added BinarySearchF

---
+++ LinqFasterer.Tests/BinarySearchTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace LinqFasterer.Tests
+{
+    public partial class Test
+    {
+        [Theory]
+        [Trait(nameof(EnumerableF.BinarySearchF), null)]
+        [InlineData(1, new [] {5,6,7,8,9}, 6)]
+        [InlineData(2, new [] {5,6,7,8,9}, 7)]
+        [InlineData(~0, new [] {5,6,7,8,9}, 0)]
+        [InlineData(~0, new [] {5,6,7,8,9}, 4)]
+        [InlineData(~5, new [] {5,6,7,8,9}, 10)]
+        [InlineData(~2, new [] {5,6,8,9}, 7)]
+        public void BinarySearchTest_Int(int expected, int[] source, int value)
+        {
+            var actual = source.BinarySearchF(value, Comparer<int>.Default);
+
+            Assert.Equal(expected, actual);
+        }
+        
+        [Theory]
+        [Trait(nameof(EnumerableF.BinarySearchF), null)]
+        [InlineData(1, new [] {5,6,7,8,9}, 0, 2, 6)]
+        [InlineData(1, new [] {5,6,7,8,9}, 1, 1, 6)]
+        [InlineData(~1, new [] {5,6,7,8,9}, 1, 0, 6)]
+        [InlineData(~2, new [] {5,6,7,8,9}, 0, 2, 7)]
+        public void BinarySearchTest_IntIndexed(int expected, int[] source, int index, int length, int value)
+        {
+            var actual = source.BinarySearchF(index, length, value, Comparer<int>.Default);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ LinqFasterer/BinarySearch.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinqFasterer
+{
+    public static partial class EnumerableF
+    {
+        /// <summary>
+        /// Searches an entire one-dimensional sorted array for a value using the specified comparer.
+        /// </summary>
+        /// <param name="source">The sorted one-dimensional, zero-based <see cref="IList{T}"/> to search.</param>
+        /// <param name="value">The object to search for.</param>
+        /// <param name="comparer">An optional <see cref="IComparer{T}"/> used to compare values; falls back to <see cref="Comparer{T}.Default"/> when set to <see langword="null"/>.</param>
+        /// <typeparam name="TSource">The type of the elements of <paramref name="source"/>.</typeparam>
+        /// <returns>
+        /// The index of the specified <paramref name="value"/> in the specified <paramref name="source"/>, if <paramref name="value"/> is found; otherwise, a negative number.
+        /// If <paramref name="value"/> is not found and <paramref name="value"/> is less than one or more elements in <paramref name="source"/>, the negative number returned is the bitwise complement of the index of the first element that is larger than <paramref name="value"/>.
+        /// If <paramref name="value"/> is not found and <paramref name="value"/> is greater than all elements in <paramref name="source"/>, the negative number returned is the bitwise complement of (the index of the last element plus 1).
+        /// If this method is called with a non-sorted <paramref name="source"/>, the return value can be incorrect and a negative number could be returned, even if <paramref name="value"/> is present in <paramref name="source"/>.
+        /// </returns>
+        public static int BinarySearchF<TSource>(this IList<TSource> source, TSource value, IComparer<TSource>? comparer = null)
+        {
+            comparer ??= Comparer<TSource>.Default;
+
+            var sourceArray = source.ToArrayF();
+
+            return Array.BinarySearch(sourceArray, value, comparer);
+        }
+        
+        /// <summary>
+        /// Searches an entire one-dimensional sorted array for a value using the specified comparer.
+        /// </summary>
+        /// <param name="source">The sorted one-dimensional, zero-based <see cref="IList{T}"/> to search.</param>
+        /// <param name="index">The starting index of the range to search.</param>
+        /// <param name="length">The length of the range to search.</param>
+        /// <param name="value">The object to search for.</param>
+        /// <param name="comparer">An optional <see cref="IComparer{T}"/> used to compare values; falls back to <see cref="Comparer{T}.Default"/> when set to <see langword="null"/>.</param>
+        /// <typeparam name="TSource">The type of the elements of <paramref name="source"/>.</typeparam>
+        /// <returns>
+        /// The index of the specified <paramref name="value"/> in the specified <paramref name="source"/>, if <paramref name="value"/> is found; otherwise, a negative number.
+        /// If <paramref name="value"/> is not found and <paramref name="value"/> is less than one or more elements in <paramref name="source"/>, the negative number returned is the bitwise complement of the index of the first element that is larger than <paramref name="value"/>.
+        /// If <paramref name="value"/> is not found and <paramref name="value"/> is greater than all elements in <paramref name="source"/>, the negative number returned is the bitwise complement of (the index of the last element plus 1).
+        /// If this method is called with a non-sorted <paramref name="source"/>, the return value can be incorrect and a negative number could be returned, even if <paramref name="value"/> is present in <paramref name="source"/>.
+        /// </returns>
+        public static int BinarySearchF<TSource>(this IList<TSource> source, int index, int length, TSource value, IComparer<TSource>? comparer = null)
+        {
+            comparer ??= Comparer<TSource>.Default;
+
+            var sourceArray = source.ToArrayF();
+
+            return Array.BinarySearch(sourceArray, index, length, value, comparer);
+        }
+    }
+}
