522beaa9fea75b7f651b6caa4997ed0ef45264ae
Kamil Monicz <kamil@monicz.pl>
2021-01-04 05:04:28 +0100

    Reduced code reuse

--- LinqFasterer/Repeat.cs
+++ LinqFasterer/Repeat.cs
@@ -13,30 +13,23 @@ namespace LinqFasterer
         /// <param name="count">The number of times to repeat the value in the generated sequence.</param>
         /// <typeparam name="TResult">The type of the value to be repeated in the result sequence.</typeparam>
         /// <returns>An <see cref="IList{T}"/> that contains a repeated <paramref name="element"/>.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static IList<TResult> RepeatF<TResult>(TResult element, int count)
         {
-            if (count < 1)
-                return EmptyF<TResult>();
-            
-            var resultLength = count;
-            var result = new TResult[resultLength];
-            var filled = Math.Min(resultLength, 16);
-            
-            // It's more efficient to fill some initial data by index instead of calling Array.Copy straightaway
-            // The magic number is a result of multiple benchmark tests
-            for (var i = 0; i < filled; i++)
-                result[i] = element;
-
-            // ReSharper disable once InvertIf
-            if (filled < resultLength)
-            {
-                for (; filled * 2 < resultLength; filled *= 2)
-                    Array.Copy(result, 0, result, filled, filled);
-
-                Array.Copy(result, 0, result, filled, resultLength - filled);
-            }
-
-            return result;
+            return count < 1 ? Array.Empty<TResult>() : new TResult[count].FillF(element, 0, count);
+        }
+        
+        /// <summary>
+        /// Generates a sequence that contains one repeated sequence.
+        /// </summary>
+        /// <param name="source">An <see cref="IList{T}"/> to be repeated.</param>
+        /// <param name="count">The number of times to repeat the <paramref name="source"/> in the generated sequence.</param>
+        /// <typeparam name="TResult">The type of the elements of <paramref name="source"/>.</typeparam>
+        /// <returns>An <see cref="IList{T}"/> that contains a repeated <paramref name="source"/>.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static IList<TResult> RepeatF<TResult>(this IList<TResult> source, int count)
+        {
+            return RepeatSequenceF(source, count);
         }
         
         /// <summary>
@@ -49,7 +42,7 @@ namespace LinqFasterer
         public static IList<TResult> RepeatSequenceF<TResult>(IList<TResult> source, int count)
         {
             if (count < 1)
-                return EmptyF<TResult>();
+                return Array.Empty<TResult>();
 
             var sourceArray = source.ToArrayF();
             var sourceLength = sourceArray.Length;
@@ -57,7 +50,7 @@ namespace LinqFasterer
             switch (sourceLength)
             {
                 case 0:
-                    return EmptyF<TResult>();
+                    return Array.Empty<TResult>();
                 case 1:
                     return RepeatF(sourceArray[0], count);
             }
@@ -74,19 +67,6 @@ namespace LinqFasterer
             Array.Copy(result, 0, result, filled, resultLength - filled);
 
             return result;
-        }
-        
-        /// <summary>
-        /// Generates a sequence that contains one repeated sequence.
-        /// </summary>
-        /// <param name="source">An <see cref="IList{T}"/> to be repeated.</param>
-        /// <param name="count">The number of times to repeat the <paramref name="source"/> in the generated sequence.</param>
-        /// <typeparam name="TResult">The type of the elements of <paramref name="source"/>.</typeparam>
-        /// <returns>An <see cref="IList{T}"/> that contains a repeated <paramref name="source"/>.</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static IList<TResult> RepeatF<TResult>(this IList<TResult> source, int count)
-        {
-            return RepeatSequenceF(source, count);
         }
     }
 }
