5c85950e8049641f0b2b8875d132ef790746eaad
Kamil Monicz <kamil@monicz.pl>
2021-01-04 05:05:17 +0100

    Added EmptyF helper extension method

--- LinqFasterer/Empty.cs
+++ LinqFasterer/Empty.cs
@@ -15,5 +15,15 @@ namespace LinqFasterer
         {
             return Array.Empty<TResult>();
         }
+        
+        /// <summary>
+        /// Returns an empty sequence that has the specified type argument.
+        /// </summary>
+        /// <returns>An empty sequence.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static IList<TResult> EmptyF<TResult>(this IList<TResult> _)
+        {
+            return Array.Empty<TResult>();
+        }
     }
 }
