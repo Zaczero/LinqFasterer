da4b98a9cb205ac957532c4f98a64df714217653
Kamil Monicz <kamil@monicz.pl>
2021-01-06 06:48:40 +0100

    Added ForEachF

---
+++ LinqFasterer.Tests/ForEachTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using LinqFasterer.Shared;
+using Xunit;
+
+namespace LinqFasterer.Tests
+{
+    public partial class Test
+    {
+        [Theory]
+        [Trait(nameof(EnumerableF.ForEachF), null)]
+        [MemberData(nameof(Utilities.TestArray), typeof(int), 5, 0, 100, MemberType = typeof(Utilities))]
+        public void ForEachTest_Int(IList<int> source)
+        {
+            var expected = source.ToArrayF();
+            var actual = new List<int>();
+
+            source.ForEachF(i => actual.Add(i));
+
+            Assert.Equal(expected, actual.ToArray());
+        }
+    }
+}
---
+++ LinqFasterer/ForEach.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinqFasterer
+{
+    public static partial class EnumerableF
+    {
+        /// <summary>
+        /// Performs the specified action on each element of the specified sequence.
+        /// </summary>
+        /// <param name="source">An <see cref="IList{T}"/> on whose elements the action is to be performed.</param>
+        /// <param name="action">An <see cref="Action{T}"/> to perform on each element of <paramref name="source"/>.</param>
+        /// <typeparam name="TSource">The type of the elements of <paramref name="source"/>.</typeparam>
+        /// <returns>The input <paramref name="source"/> sequence.</returns>
+        public static IList<TSource> ForEachF<TSource>(this IList<TSource> source, Action<TSource> action)
+        {
+            var sourceArray = source.ToArrayF();
+            var sourceLength = sourceArray.Length;
+
+            for (var i = 0; i < sourceLength; i++)
+                action(sourceArray[i]);
+
+            return sourceArray;
+        }
+    }
+}
