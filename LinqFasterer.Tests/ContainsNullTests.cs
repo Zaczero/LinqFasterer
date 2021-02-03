c20e5bf15f64db9c9905ce25c16b370a6915e67c
Kamil Monicz <kamil@monicz.pl>
2021-02-03 09:32:48 +0100

    Added ContainsNullF

---
+++ LinqFasterer.Tests/ContainsNullTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using LinqFasterer.Shared;
+using Xunit;
+
+namespace LinqFasterer.Tests
+{
+    public partial class Test
+    {
+        [Theory]
+        [Trait(nameof(EnumerableF.ContainsNullF), null)]
+        [MemberData(nameof(Utilities.TestArray), typeof(int), 5, 0, 100, MemberType = typeof(Utilities))]
+        [MemberData(nameof(Utilities.TestArray), typeof(int), 10, 0, 2, MemberType = typeof(Utilities))]
+        public void ContainsNullTest_IntFalse(IList<int> source)
+        {
+            Assert.False(source.ContainsNullF());
+        }
+        
+        [Theory]
+        [Trait(nameof(EnumerableF.ContainsNullF), null)]
+        [InlineData(new object[] {new object[] {1,2,3,4,5}})]
+        public void ContainsNullTest_ObjectFalse(IList<object> source)
+        {
+            Assert.False(source.ContainsNullF());
+        }
+        
+        [Theory]
+        [Trait(nameof(EnumerableF.ContainsNullF), null)]
+        [InlineData(new object[] {new object[] {1,2,3,null,5}})]
+        public void ContainsNullTest_ObjectTrue(IList<object> source)
+        {
+            Assert.True(source.ContainsNullF());
+        }
+    }
+}
---
+++ LinqFasterer/ContainsNull.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinqFasterer
+{
+    public partial class EnumerableF
+    {
+        /// <summary>
+        /// Determines whether a sequence contains a null element.
+        /// </summary>
+        /// <param name="source">An <see cref="IList{T}"/> in which to check for a null.</param>
+        /// <typeparam name="TSource">The type of the elements of <paramref name="source"/>.</typeparam>
+        /// <returns><see langword="true"/> if <paramref name="source"/> contains a null element; otherwise, <see langword="false"/>.</returns>
+        public static bool ContainsNullF<TSource>(this IList<TSource> source)
+        {
+            if (default(TSource) != null)
+                return false;
+            
+            var sourceArray = source.ToArrayF();
+            var sourceLength = sourceArray.Length;
+            
+            for (var i = 0; i < sourceLength; i++)
+                if (sourceArray[i] == null)
+                    return true;
+
+            return false;
+        }
+    }
+}
